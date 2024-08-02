7309247ffded9a8eb92964a6f64105b3b3a4cb0f
kauaserafim <kauaserafimxx@gmail.com>
2024-08-02 16:53:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let idade = 17;
+
+let nome = "kauã";
+
+let ativo = true;
+
+console.log("Idade: ", idade);
+console.log("Nome: ", nome);
+console.log("Ativo: ", ativo);
+
+function somar(a, b) {
+    return a + b;
+    }
+    console.log(somar(5, 3));
+
+    document.getElementById('meuBotao').addEventListener('click', function() {
+        alert('Você clicou no botão!');
+        });
